7ddcf9718d9943f604ea9620779c51c68fb12a0d
KitUrbano <admin@kiturbano.com>
2015-12-01 13:14:42 -0300

    Update MyService.java

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -34,7 +34,7 @@ public class MyService extends BackgroundService {
 	
 	private final static String TAG = MyService.class.getSimpleName();
 	
-	private String mHelloTo = "World ESTEBAN";
+	private String mHelloTo = "World ESTEBAN PEREZ";
 
 	@Override
 	protected JSONObject doWork() {
